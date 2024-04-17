45a32794d97e2ca35bcddcdeacd8259ad436eaac
S8ight <fs.v1835@gmail.com>
2024-04-17 11:32:12 +0300

    Removed exceptions handling from methods and added null check expressions

--- ApiKeysApi/Services/ApiKeyService.cs
+++ ApiKeysApi/Services/ApiKeyService.cs
@@ -20,56 +20,54 @@ public class ApiKeyService : IApiKeyService
         _mapper = mapper;
     }
 
-    public async Task<ApiKey?> GetApiKeyByIdAsync(int id)
+    public async Task<ApiKey> GetApiKeyByIdAsync(int id)
     {
-        return await _apiKeyRepository.GetApiKeyByIdAsync(id);
+        var apiKey = await _apiKeyRepository.GetApiKeyByIdAsync(id);
+
+        if (apiKey == null)
+        {
+            throw new KeyNotFoundException("ApiKey with provided id not found.");
+        }
+
+        return apiKey;
     }
 
-    public async Task<ApiKey?> GetApiKeyByHashAsync(string apiKeyHash)
+    public async Task<ApiKey> GetApiKeyByHashAsync(string apiKeyHash)
     {
-        return await _apiKeyRepository.GetApiKeyByHashAsync(apiKeyHash);
+        var apiKey = await _apiKeyRepository.GetApiKeyByHashAsync(apiKeyHash);
+        
+        if (apiKey == null)
+        {
+            throw new KeyNotFoundException("ApiKey with provided hash not found.");
+        }
+
+        return apiKey;
     }
 
     public async Task<string> AddApiKeyAsync(ApiKeyRequest apiKeyRequest)
     {
-        try
-        {
-            var apiKey = _mapper.Map<ApiKeyRequest, ApiKey>(apiKeyRequest);
-            apiKey.ApiKeyHash = GenerateApiKeyHash();
+        var apiKey = _mapper.Map<ApiKeyRequest, ApiKey>(apiKeyRequest);
+        apiKey.ApiKeyHash = GenerateApiKeyHash();
 
-            await _apiKeyRepository.AddApiKeyAsync(apiKey);
-            _logger.LogInformation($"ApiKey for user(id: {apiKey.UserId}) has been added");
+        await _apiKeyRepository.AddApiKeyAsync(apiKey);
+        _logger.LogInformation($"ApiKey for user(id: {apiKey.UserId}) has been added.");
 
-            return apiKey.ApiKeyHash;
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "Error occurred while adding ApiKey");
-            throw;
-        }
+        return apiKey.ApiKeyHash;
     }
-    
+
     public async Task UpdateAlbumAsync(int id, ApiKeyUpdateRequest request)
     {
-        try
+        var existingApiKey = await _apiKeyRepository.GetApiKeyByIdAsync(id);
+
+        if (existingApiKey == null)
         {
-            var existingApiKey = await _apiKeyRepository.GetApiKeyByIdAsync(id);
-            
-            if (existingApiKey == null)
-            {
-                throw new KeyNotFoundException($"ApiKey with Id: {id} not found.");
-            }
-            
-            _mapper.Map(request, existingApiKey);
-            await _apiKeyRepository.UpdateApiKeyAsync(existingApiKey);
+            throw new KeyNotFoundException($"ApiKey with Id: {id} not found.");
         }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "An error occurred while updating ApiKey");
-            throw;
-        }
+
+        _mapper.Map(request, existingApiKey);
+        await _apiKeyRepository.UpdateApiKeyAsync(existingApiKey);
     }
-    
+
     string GenerateApiKeyHash()
     {
         const int apiKeyLength = 64;
