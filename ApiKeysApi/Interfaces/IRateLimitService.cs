f0d3e178c011e6ac40b9bcebce2d0b7ca6796de8
S8ight <fs.v1835@gmail.com>
2024-04-24 16:33:45 +0300

    Added service to check request rate limits using Redis and sliding window algorithm

---
+++ ApiKeysApi/Interfaces/IRateLimitService.cs
@@ -0,0 +1,8 @@
+﻿using ApiKeysApi.DataAccess.Entities;
+
+namespace ApiKeysApi.Interfaces;
+
+public interface IRateLimitService
+{
+    Task<bool> IsRequestAllowed(string apiKey, int rateLimitSecond, int rateLimitMinute, int rateLimitHour, int rateLimitDay);
+}
---
+++ ApiKeysApi/Services/RedisRateLimitService.cs
@@ -0,0 +1,50 @@
+﻿using ApiKeysApi.DataAccess.Entities;
+using ApiKeysApi.Interfaces;
+using StackExchange.Redis;
+
+namespace ApiKeysApi.Services;
+
+public class RedisRateLimitService : IRateLimitService
+{
+    private readonly IConnectionMultiplexer _redis;
+
+    public RedisRateLimitService(IConnectionMultiplexer redis)
+    {
+        _redis = redis;
+    }
+
+    public async Task<bool> IsRequestAllowed(string apiKey, int rateLimitSecond, int rateLimitMinute, int rateLimitHour,
+        int rateLimitDay)
+    {
+        var db = _redis.GetDatabase();
+        var tasks = new List<Task<bool>>();
+
+        var periods = new[]
+        {
+            new { Interval = TimeSpan.FromSeconds(1), Limit = rateLimitSecond },
+            new { Interval = TimeSpan.FromMinutes(1), Limit = rateLimitMinute },
+            new { Interval = TimeSpan.FromHours(1), Limit = rateLimitHour },
+            new { Interval = TimeSpan.FromDays(1), Limit = rateLimitDay }
+        };
+
+        foreach (var period in periods)
+        {
+            var key = $"ratelimit:{apiKey}:{period.Interval.TotalSeconds}";
+            var now = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            
+            var tran = db.CreateTransaction();
+            tran.AddCondition(Condition.KeyExists(key));
+            
+            _ = tran.SortedSetAddAsync(key, now.ToString(), now);
+            _ = tran.SortedSetRemoveRangeByScoreAsync(key, double.NegativeInfinity, now - period.Interval.TotalSeconds);
+            var countTask = tran.SortedSetLengthAsync(key);
+            _ = tran.KeyExpireAsync(key, period.Interval);
+            
+            tasks.Add(tran.ExecuteAsync().ContinueWith(t => countTask.Result <= period.Limit));
+        }
+        
+        var results = await Task.WhenAll(tasks);
+        return results.All(result => result);
+    }
+
+}
