9c2754157d5af2b3e46553b8ee731572b8bdfbde
S8ight <fs.v1835@gmail.com>
2024-04-26 17:04:29 +0300

    Registered new services and middleware

--- ApiKeysApi/Program.cs
+++ ApiKeysApi/Program.cs
@@ -1,16 +1,10 @@
-using System.Text;
 using ApiKeysApi.DataAccess.DbContexts;
 using ApiKeysApi.DataAccess.Repository;
 using ApiKeysApi.Interfaces;
 using ApiKeysApi.Middleware;
-using ApiKeysApi.Policies.ApiKeyOrTokenPolicy;
-using ApiKeysApi.Policies.ApiKeyPolicy;
 using ApiKeysApi.Services;
 using ApiKeysApi.Validators;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using Serilog;
 using Serilog.Events;
 using StackExchange.Redis;
@@ -22,7 +16,7 @@ builder.Services.AddDbContext<ApiKeysDbContext>(options =>
     options.UseSqlServer(builder.Configuration["ConnectionStrings:DataBaseConnection"]);
 });
 
-builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(
+builder.Services.AddSingleton<IConnectionMultiplexer>(sp => ConnectionMultiplexer.Connect(
     builder.Configuration["ConnectionStrings:RedisConnection"] ?? string.Empty ));
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
@@ -31,39 +25,15 @@ builder.Host.UseSerilog((context, configuration) =>
     configuration.ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddTransient<IApiKeyRepository, ApiKeyRepository>();
+builder.Services.AddTransient<IUserRepository, UserRepository>();
 builder.Services.AddTransient<IApiKeyService, ApiKeyService>();
+builder.Services.AddTransient<ITokenService, JwtTokenService>();
+builder.Services.AddTransient<IUserService, UserService>();
 builder.Services.AddTransient<IRateLimitService, RedisRateLimitService>();
 builder.Services.AddTransient<IJwtTokenValidator, JwtTokenValidator>();
 builder.Services.AddTransient<IApiKeyValidator, ApiKeyValidator>();
 
 builder.Services.AddHttpContextAccessor();
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = false,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            RequireExpirationTime = true,
-            RequireSignedTokens = true,
-            ClockSkew = TimeSpan.Zero,
-            ValidIssuer = builder.Configuration["JwtTokenConfiguration:Issuer"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtTokenConfiguration:AccessTokenKey"] ?? string.Empty))
-        };
-    });
-
-builder.Services.AddAuthorizationBuilder()
-    .AddPolicy("RequireApiKey", policy =>
-        policy.AddRequirements(new ApiKeyRequirement()))
-    /*.AddPolicy("RequireApiKeyOrToken", policy =>
-        policy.AddRequirements(new ApiKeyOrTokenRequirement()))*/;
-
-builder.Services.AddScoped<IAuthorizationHandler, ApiKeyHandler>();
-builder.Services.AddScoped<IAuthorizationHandler, ApiKeyOrTokenHandler>();
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -91,6 +61,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
+app.UseMiddleware<AuthorizationHandlingMiddleware>();
 app.UseMiddleware<RequestLoggingMiddleware>();
 
 app.Run();
