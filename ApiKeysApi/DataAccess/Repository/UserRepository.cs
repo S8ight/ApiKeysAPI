57e8320468345550e0018d3ee6ca667280c0c926
S8ight <fs.v1835@gmail.com>
2024-04-26 16:52:44 +0300

    Added repository for user

---
+++ ApiKeysApi/DataAccess/Repository/UserRepository.cs
@@ -0,0 +1,49 @@
+﻿using ApiKeysApi.DataAccess.DbContexts;
+using ApiKeysApi.DataAccess.Entities;
+using ApiKeysApi.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiKeysApi.DataAccess.Repository;
+
+public class UserRepository : IUserRepository
+{
+    private readonly ApiKeysDbContext _context;
+
+    public UserRepository(ApiKeysDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User?> GetUserByUserNameAsync(string userName)
+    {
+        return await _context.Users.FirstOrDefaultAsync(u => u.UserName == userName);
+    }
+    
+    public async Task<User?> GetUserByIdAsync(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+
+    public async Task AddUserAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+        await _context.SaveChangesAsync();
+    }
+    
+    public async Task DeleteUserAsync(User user)
+    {
+        _context.Users.Remove(user);
+        await _context.SaveChangesAsync();
+    }
+    
+    public async Task UpdateUserAsync(User user)
+    {
+        _context.Users.Update(user);
+        await _context.SaveChangesAsync();
+    }
+    
+    public IQueryable<User?> GetUsers()
+    {
+        return _context.Users.AsQueryable();
+    }
+}
---
+++ ApiKeysApi/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using ApiKeysApi.DataAccess.Entities;
+
+namespace ApiKeysApi.Interfaces;
+
+public interface IUserRepository
+{
+    Task<User?> GetUserByUserNameAsync(string userName);
+    Task<User?> GetUserByIdAsync(int id);
+    Task AddUserAsync(User user);
+    Task DeleteUserAsync(User user);
+    Task UpdateUserAsync(User user);
+    IQueryable<User?> GetUsers();
+}
