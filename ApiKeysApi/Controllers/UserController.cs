fdc50b2c2c964e899907b84c36588bbb931087e4
S8ight <fs.v1835@gmail.com>
2024-04-26 17:02:23 +0300

    Created endpoints for users

---
+++ ApiKeysApi/Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using ApiKeysApi.DTOs.Request;
+using ApiKeysApi.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiKeysApi.Controllers;
+
+[ApiController]
+[Route("/api/v1/users")]
+public class UserController : ControllerBase
+{
+    private readonly IUserService _userService;
+    
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+    
+    [AllowAnonymous]
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
+    {
+        var user = await _userService.UserLogin(loginRequest);
+        if (user == null)
+        {
+            return Unauthorized();
+        }
+
+        return Ok(user);
+    }
+    
+    [HttpPost("create")]
+    public async Task<IActionResult> CreateUser([FromBody] UserRequest userRequest)
+    {
+        await _userService.CreateUser(userRequest);
+        
+        return Ok();
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetUserById(int id)
+    {
+        var user = await _userService.GetUserById(id);
+        
+        return Ok(user);
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetUsers([FromQuery] PaginationRequest paginationRequest)
+    {
+        var users = await _userService.GetUsers(paginationRequest);
+        
+        return Ok(users);
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(int id)
+    {
+        await _userService.DeleteUser(id);
+        
+        return Ok();
+    }
+}
